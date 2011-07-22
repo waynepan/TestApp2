daee8347cef3640fc461d1036a7a99d5309f8699
Wayne Pan <waynepan@Waynes-MacBook-Air.local>
2011-07-21 17:43:30 -0700

    Initial Commit

---
+++ TestApp2/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  TestApp2
+//
+//  Created by Wayne Pan on 7/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ TestApp2Tests/TestApp2Tests.h
@@ -0,0 +1,13 @@
+//
+//  TestApp2Tests.h
+//  TestApp2Tests
+//
+//  Created by Wayne Pan on 7/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestApp2Tests : SenTestCase
+
+@end
